52abee7d2f2c15a4e2d11a13afe758798cfdd3e2
omoleewotheophilus-omotech <omoleewotheophilus@gmail.com>
2025-11-10 12:27:28 -0500

    Add button click alert for contact demo

---
+++ script.js
@@ -0,0 +1,6 @@
+// Buttons alert for demo
+document.querySelectorAll("button, a").forEach((btn) => {
+  btn.onclick = function() {
+    alert("Thanks for contacting Omotech! We will get back to you soon.");
+  };
+});
